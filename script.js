ba0fc84c3602a072c539f1f62059c40b2290cbae
Albert Sun <albert.h.sun@gmail.com>
2021-02-26 19:33:34 -0500

    script.js - Shamelessly added donation stuff

--- script.js
+++ script.js
@@ -15,7 +15,8 @@
 // 1.5.0 - Changed workings to check rendered button color, hopefully works now
 // 1.6.0 - Changed polling to be on set interval instead of on mutation (now it works!)
 
-const title = "Best Buy (Product Details) Automation by akito#9528";
+const title = "Best Buy (Product Details) Automation by akito#9528 / Albert Sun";
+const donationText = "Thank you! | Bitcoin: bc1q6u7kalsxunl5gleqcx3ez4zn6kmahrsnevx2w4 / 1KgcytPHXNwboNbXUN3ZyuASDZWt8Qcf1t | Paypal: akitocodes@gmail.com";
 const keepChecking = false; // Whether to keep polling on page once item is added to cart        | Default: false
 const pollInterval = 500;   // In milliseconds, interval between button pollign for availability | Default: 500
 
@@ -28,12 +29,28 @@ function buttonAvailable(button) {
 (function() {
     'use strict'; // necessary?
 
-    // initialize bottom status banner
-    let banner = document.createElement("div");
+    // initialize bottom status + donation banner
+    const banner = document.createElement("div");
     banner.style.position = "fixed"; banner.style.bottom = "0px"; banner.style.zIndex = 100;
     banner.style.width = "100%"; banner.style.padding = "4px"; banner.style.textAlign = "center";
     banner.style.backgroundImage = "linear-gradient(to right, coral, crimson, coral, crimson)";
-    banner.innerHTML = `${title} | Initializing script`;
+    banner.style.fontFamily = "Verdana"; banner.style.fontSize = "12px";
+    banner.style.display = "flex"; banner.style.flexDirection = "row"; banner.style.justifyContent = "space-between";
+    banner.style.alignItems = "center";
+
+    // initialize left status banner
+    let statusInfo = document.createElement("div");
+    statusInfo.style.textAlign = "left"; statusInfo.style.paddingLeft = "10px";
+    statusInfo.style.order = 0; statusInfo.style.flexBasis = "40%"; // set as child, index 0, 1/4 size
+    statusInfo.innerText = `${title} | Initializing script`;
+    banner.appendChild(statusInfo);
+
+    // initialize right donation banner
+    let donationInfo = document.createElement("div");
+    donationInfo.style.textAlign = "right"; donationInfo.style.paddingRight = "10px";
+    donationInfo.style.order = 1; donationInfo.style.flexBasis = "60%"; // set as child, index 0, 1/4 size
+    donationInfo.innerText = donationText;
+    banner.appendChild(donationInfo);
 
     // Mutation Observer to detect HTML changes to the "Add to Cart" / "Please Wait" button
     // The script waits until the button color changes from grey (Please Wait) back to yellow (Add to Cart).
@@ -47,7 +64,7 @@ function buttonAvailable(button) {
         const cartButton = document.getElementsByClassName("add-to-cart-button")[0];
         const soldOut = cartButton.classList.contains("btn-disabled"); // disabled button
         let initAvailable = buttonAvailable(cartButton);
-        banner.innerHTML = soldOut ? `${title} | Product currently sold out, script non-functioning`
+        statusInfo.innerHTML = soldOut ? `${title} | Product currently sold out, script non-functioning`
             : initAvailable ? `${title} | Add button clickable, please click to initialize product queue`
             : `${title} | Existing product queue detected, waiting for button availability change`;
         if(soldOut) { // don't run script on sold out products
@@ -59,12 +76,12 @@ function buttonAvailable(button) {
             console.log(window.getComputedStyle(cartButton, null).getPropertyValue("background-color"), initAvailable, currentAvailable);
             if(initAvailable === true && currentAvailable === false) {
                 // button clicked to enter queue (yellow => grey)
-                banner.innerHTML = `${title} | Queue entry detected, waiting for button availability change`;
+                statusInfo.innerHTML = `${title} | Queue entry detected, waiting for button availability change`;
 
                 initAvailable = false;
             } else if(initAvailable === false && currentAvailable === true) { // product now available from queue
                 // product avialable (grey => yellow)
-                banner.innerHTML = `${title} | Availability change detected, button clicked and window opened! Good luck!`;
+                statusInfo.innerHTML = `${title} | Availability change detected, button clicked and window opened! Good luck!`;
 
                 // click button, play audio, and open window
                 cartButton.click();
