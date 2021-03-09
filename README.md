eefb2d46077aa850f39694452398a5831579aa7d
albert-sun <albert.h.sun@gmail.com>
2021-03-09 11:05:15 -0500

    new script?

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Simple Tampermonkey script for automating Best Buy's product queue.
 **SOMEWHAT UNTESTED, if you're feeling safe feel free to download 1.7.1 [here](https://github.com/albert-sun/bestbuy-queue-automation/tree/ad80072d2ee0d8a96ade54eb1f9d6110fabdbb8e)**
 
 # Frequently Asked Questions
-**Q: Does this script work on non-focused tabs?**
+**Q: Does this script work on non-focused tabs / multiple tabs?**
 A: The script runs as long as the tab has been focused once (for instance, if you click "Open in new tab" you would have to navigate to the tab). The script also only works during the queue, so make sure to press the initial "Add to Cart" button to initiate the queue (you should see the bottom left status change).  
 
 **Q: How do I know the script is running?**
@@ -11,7 +11,9 @@ A: You can either check the TamperMonkey or GreaseMonkey icon (it should have a 
 
 # Instructions
 Install the script through the TamperMonkey extension for Chrome or the GreaseMonkey extension for Firefox.  
-[Copy the contents of the script here as a new script into the above extensions](https://github.com/albert-sun/bestbuy-queue-automation/blob/main/script.js).  
-- The script automatically runs on page load (a red-orange banner should appear)
-- You only have to press the initial "Add to Cart" button if you don't have initialClick enabled in the script config.
-- At that point, when the queue pops the button will automatically click, play a sound, and open the cart window!
+Copy the contents of the below scripts as new scripts into the downloaded extension.  
+>> [Download the product details page script here](https://github.com/albert-sun/bestbuy-queue-automation/blob/main/script_product.js) <<
+>> [Download the cart saved items script here](https://github.com/albert-sun/bestbuy-queue-automation/blob/main/script_cart.js) <<
+
+- The scripts automatically run on page load (a red-orange banner should appear on the bottom of the page).
+- When the queue pops the button will automatically click, play a sound, and either open or refresh the cart window (depending on script)!
---
+++ script_cart.js
@@ -0,0 +1,143 @@
+// ==UserScript==
+// @name         Best Buy Automation (Cart Saved Items)
+// @namespace    akito
+// @version      1.0.0
+// @description  Auto-presses drops when button changes to Add
+// @author       akito#9528 / Albert Sun
+// @match        https://www.bestbuy.com/cart
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+/* globals $ */
+
+// Version Changelog
+// 1.0.0 - Initial release, clunky reload on successful item addition because difficult to detect DOM unload
+
+const version = "1.0.0";
+const scriptDesc = `Best Buy Automation (Cart Saved Items) v${version} by akito#9528 / Albert Sun`;
+const donationText = "Thank you! | Bitcoin: bc1q6u7kalsxunl5gleqcx3ez4zn6kmahrsnevx2w4 / 1KgcytPHXNwboNbXUN3ZyuASDZWt8Qcf1t | Paypal: akitocodes@gmail.com";
+
+// Quick and dirty script configuration, feel free to edit this!
+// - Ignore warnings with the /!\ symbol, just some whitespace linting stuff
+// - Note that auto-reload might not work properly if Chrome / Firefox unloads resources or something
+const scriptConfig = {
+    pollInterval: 0.5, // In seconds, interval between periodic button polls when checking for availability              | Default: 0.5
+    // ======================================================================================================================================
+    // Keyword inclusion whitelist for initial clicking and auto-reloading, delete or make empty array for script to ignore.
+    // Default: ["3060", "3070", "3080", "3090", "6800", "6900", "5600X", "5800X", "5900X", "5950X", "PS5"]
+    keywords: ["3060", "3070", "3080", "3090", "6800", "6900", "5600X", "5800X", "5900X", "5950X", "PS5"]
+}
+
+// Quick and dirty function which checks whether the given button / div is yellow, white, or blue.
+// These usually mean that the item is cartable (tested for product detail page and cart page).
+function buttonClickable(button) {
+    const rgb = window.getComputedStyle(button, null).getPropertyValue("background-color");
+
+    return rgb.startsWith("rgb(255") || (rgb.startsWith("rgb(0") && !rgb.startsWith("rgb(0, 0"));
+}
+
+// Checks whether the given word (string) contains any of the given keywords (substrings).
+// Automatically returns false if the keywords parameter is not an array.
+function containsSubstring(word, keywords) {
+    if(Array.isArray(keywords) === false) {
+        return false;
+    }
+
+    for(const keyword of keywords) {
+        if(word.includes(keyword)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+(async function() {
+    'use strict'; // Necessary? Comes default with Tampermonkey scripts
+
+    // Initialize bottom banner for status + donation info
+    const banner = document.createElement("div");
+    banner.style.position = "fixed"; banner.style.bottom = "0px"; banner.style.zIndex = 100;
+    banner.style.width = "100%"; banner.style.padding = "6px"; banner.style.alignItems = "center";
+    banner.style.backgroundImage = "linear-gradient(to right, coral, crimson, coral, crimson)";
+    banner.style.fontFamily = "Verdana"; banner.style.fontSize = "12px";
+    banner.style.display = "flex"; banner.style.flexDirection = "row"; banner.style.justifyContent = "space-between";
+
+    // Initialize status info (left side of bottom banner)
+    const statusInfo = document.createElement("div");
+    statusInfo.style.textAlign = "left"; statusInfo.style.paddingLeft = "10px";
+    statusInfo.style.order = 0; statusInfo.style.flexBasis = "50%";
+    statusInfo.innerText = `${scriptDesc} | Initializing script`;
+
+    // Initialize donation info (right side of bottom banner)
+    const donationInfo = document.createElement("div");
+    donationInfo.style.textAlign = "right"; donationInfo.style.paddingRight = "10px";
+    donationInfo.style.order = 1; donationInfo.style.flexBasis = "50%";
+    donationInfo.innerText = donationText;
+
+    // Actual bulk of the script including the auto-reloader and interval poller
+    // Could use document-end instead of document-start and DOM load but I dunno what's better
+    document.addEventListener("DOMContentLoaded", async function() {
+        document.body.append(banner);
+        banner.appendChild(statusInfo);
+        banner.appendChild(donationInfo);
+
+        // Preload audio because doesn't preload when tab not focused
+        const audio = new Audio("https://proxy.notificationsounds.com/notification-sounds/definite-555/download/file-sounds-1085-definite.mp3");
+
+        // Periodically poll until saved item elements exist
+        // Then, load their buttons along with product descriptions
+        statusInfo.innerHTML = `${scriptDesc} | Waiting until saved item elements are loaded ...`;
+        let productButtons;
+        do { // Check whether saved items visible (> 0, otherwise keeps running?)
+            productButtons = $(".saved-items__card-wrapper .btn.btn-block").toArray();
+            await new Promise(r => setTimeout(r, 100));
+        } while(productButtons.length === 0);
+        const productNames = $(".saved-items__card-wrapper .simple-item__description").toArray();
+
+        // Check all saved items for auto-click and queue addition
+        const productsQueue = []; // Elements to periodically poll for queue
+        for(const index in productNames) {
+            statusInfo.innerHTML = `${scriptDesc} | Analyzing ${Number(index)+1}/${productButtons.length} saved items from cart page.`;
+
+            const addButton = productButtons[index];
+            let available = buttonClickable(addButton); // Change product page too
+            const disabled = !addButton.classList.contains("button-state-default"); // Coming soon or sold out
+            const productDesc = productNames[index].innerText;
+
+            // Check whether to initially click add button by checking keyword
+            const hasKeyword = containsSubstring(productDesc, scriptConfig.keywords);
+            if(hasKeyword === true && disabled === false && available === true) { // Whitelisted, unqueued, and clickable
+                addButton.click();
+                await new Promise(r => setTimeout(r, 250)); // Wait for click to propogate
+                available = !buttonClickable(addButton); // Check whether entered into queue
+            } // Once clicked, check whether product is now added to queue or something
+            if(disabled === false && available === false) { // Currently in queue
+                productsQueue.push({
+                    button: addButton,
+                    description: productDesc,
+                });
+            }
+        }
+
+        statusInfo.innerHTML = `${scriptDesc} | Analysis complete, ${productsQueue.length} of ${productButtons.length} items queued, polling ...`;
+
+        // Initialize polling interval for each product currently queued
+        // Stop polling either when queue pops or element doesn't exist (unloaded)
+        for(const queued of productsQueue) {
+            let intervalID = setInterval(async function() {
+                const nowAvailable = buttonClickable(queued.button);
+                if(nowAvailable === true) { // queue popped, add to cart
+                    statusInfo.innerHTML = `${scriptDesc} | Add button detected and clicked (hopefully everything works fine)!`;
+
+                    // Click button, play audio, and open cart window
+                    queued.button.click();
+                    audio.play();
+
+                    // Force reload since most convenient
+                    await new Promise(r => setTimeout(r, 1500));
+                    window.location.reload()
+                }
+            }, scriptConfig.pollInterval * 1000);
+        }
+    });
+}());
