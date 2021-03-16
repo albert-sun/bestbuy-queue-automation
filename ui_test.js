4f6e2c075d1f0950e28502c203e12fe0e2f04094
albert-sun <albert.h.sun@gmail.com>
2021-03-16 12:42:53 -0400

    ui update before best buy

--- ui_test.js
+++ ui_test.js
@@ -15,7 +15,18 @@ const loggingDiv = generateWindow(
     400
 );
 
+const settings = {
+    keepPolling: {description: "Keep Polling", type: "boolean", value: false},
+    initialClick: {description: "Initial Click", type: "boolean", value: false},
+    soldOutReload: {description: "Sold Out Reload", type: "boolean", value: false},
+    pollInterval: {description: "Polling Interval", type: "number", value: 500},
+    initialDelay: {description: "Initial Delay", type: "number", value: 500},
+    soldOutDelay: {description: "Sold Out Delay", type: "number", value: 60000},
+} 
+
 const func = designateLogging(loggingDiv);
 setInterval(function() {
+    console.log(settings.a)
     func("woo")
 }, 500);
+designateSettings(settingsDiv, settings);
--- user_interface.js
+++ user_interface.js
@@ -133,13 +133,98 @@ function generateInterface(scriptText, messageText) {
     });
 }
 
+// Small function for "weighing" the settings type for sorting
+// @param {Object} value
+function settingsWeight(value) {
+    switch(value.type) {
+        case "boolean": return 0;
+        case "number": return 1;
+        case "array": return 2;
+    }
+}
+
+// Designates div for settings with onchange modification to passed settings
+// number: current (number), valid (array of numbers)
+// @param {DOMElement} contentDiv
+// @param {Object} settings
+function designateSettings(contentDiv, settings) {
+    // Generate wrapper table element
+    const settingsTable = document.createElement("table");
+    contentDiv.appendChild(settingsTable);
+    settingsTable.classList.add("akito-table");
+
+    // Transform and sort settings by type and alphabetical order 
+    const settingsArray = [];
+    for(const property in settings) {
+        settingsArray.push(settings[property]);
+    }
+    settingsArray.sort(function(value, value2) {
+        const valueWeight = settingsWeight(value);
+        const value2Weight = settingsWeight(value2);
+
+        if(valueWeight < value2Weight) {
+            return -1;
+        } else if(valueWeight === value2Weight) {
+            if(value.description < value2.description) {
+                return -1;
+            } else if(value.description === value2.description) {
+                return 0; // Should never happen
+            } else {
+                return 1;
+            }
+        } else {
+            return 1;
+        }
+    });
+
+    // Add each setting as new row within table and attach onchange
+    for(const setting of settingsArray) {
+        // Generate row for specific setting
+        const row = document.createElement("tr");
+        settingsTable.appendChild(row);
+
+        // Generate cell showing setting description (onhover?)
+        const descriptionCell = document.createElement("td");
+        row.appendChild(descriptionCell);
+        descriptionCell.innerHTML = `<b>${setting.description}</b>`;        
+
+        // Generate cell with actual switcher (checkbox, slider, etc.)
+        // Currently no support for arrays because they're complicated
+        const settingCell = document.createElement("td");
+        row.appendChild(settingCell);
+        settingCell.style.align = "center";
+        switch(setting.type) {
+            case "boolean": // Checkbox
+                const checkbox = document.createElement("input");
+                row.appendChild(checkbox);
+                checkbox.setAttribute("type", "checkbox");
+                checkbox.checked = setting.value;
+                checkbox.onclick = function() {
+                    setting.value = checkbox.checked;
+                };
+                break;
+            case "number": // Numerical slider input + display
+                const numberInput = document.createElement("input");
+                row.appendChild(numberInput);
+                numberInput.setAttribute("type", "number");
+                numberInput.value = setting.value;
+                numberInput.onclick = function() {
+                    setting.value = numberInput.value;
+                };
+                break;
+            case "array": break; // Currently not implemented
+        }
+    }
+}
+
 // Designates div for logging and generates table appending function
 // @param {DOMElement} contentDiv
 // @returns {function}
 function designateLogging(contentDiv) {
+    // Generate wrapper table element
     const loggingTable = document.createElement("table");
     contentDiv.appendChild(loggingTable);
-    loggingTable.classList.add("akito-loggingTable");
+    loggingTable.classList.add("akito-table");
 
     // Generates timestamp and appends to logging table
     // @param {string} message
